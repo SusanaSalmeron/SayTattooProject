12447175769665ae8cd94bba566191e7106f7a29
Susana Salmeron <exdream76@gmail.com>
2021-06-12 18:49:52 +0200

    Cambios visuales en el formulario

--- proyectoFinalAngular/src/app/components/formulario-registro/formulario-registro.component.ts
+++ proyectoFinalAngular/src/app/components/formulario-registro/formulario-registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'formulario-registro',
@@ -12,18 +12,42 @@ export class FormularioRegistroComponent implements OnInit {
   //TODO validaciones del formulario
   constructor() {
     this.formulario = new FormGroup({
-      nombre: new FormControl(''),
-      apellidos: new FormControl(''),
-      direccion: new FormControl(''),
-      ciudad: new FormControl(''),
-      cp: new FormControl(''),
-      telefono: new FormControl(''),
-      fechaNacimiento: new FormControl(''),
-      NombreUsuario: new FormControl(''),
-      Email: new FormControl(''),
-      Password: new FormControl(''),
-      repetirPassword: new FormControl(''),
-      tatuador: new FormControl('')
+      nombre: new FormControl('', [
+        Validators.required
+      ]),
+      apellidos: new FormControl('', [
+        Validators.required
+      ]),
+      direccion: new FormControl('', [
+        Validators.required
+      ]),
+      ciudad: new FormControl('', [
+        Validators.required
+      ]),
+      cp: new FormControl('', [
+        Validators.required
+      ]),
+      telefono: new FormControl('', [
+        Validators.required
+      ]),
+      fechaNacimiento: new FormControl('', [
+        Validators.required
+      ]),
+      NombreUsuario: new FormControl('', [
+        Validators.required
+      ]),
+      Email: new FormControl('', [
+        Validators.required
+      ]),
+      Password: new FormControl('', [
+        Validators.required
+      ]),
+      repetirPassword: new FormControl('', [
+        Validators.required
+      ]),
+      tatuador: new FormControl('', [
+        Validators.required
+      ])
     })
 
   }
