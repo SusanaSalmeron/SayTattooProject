8d593a138b3f64c9bff7a20b725113e59f12f0ec
Susana Salmeron <exdream76@gmail.com>
2021-06-14 18:30:50 +0200

    Validadores del formulario registro,
    excepto la validacion por edad

--- proyectoFinalAngular/src/app/components/formulario-registro/formulario-registro.component.ts
+++ proyectoFinalAngular/src/app/components/formulario-registro/formulario-registro.component.ts
@@ -1,5 +1,7 @@
+import { INFERRED_TYPE } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'formulario-registro',
@@ -9,13 +11,15 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class FormularioRegistroComponent implements OnInit {
   formulario: FormGroup;
 
-  //TODO validaciones del formulario
+
+  //TODO validaciones del formulario -fecha nacimiento = mayor edad
   constructor() {
     this.formulario = new FormGroup({
       nombre: new FormControl('', [
-        Validators.required
+        Validators.required,
+        Validators.minLength(5)
       ]),
-      apellidos: new FormControl('', [
+      sexo: new FormControl('', [
         Validators.required
       ]),
       direccion: new FormControl('', [
@@ -25,37 +29,69 @@ export class FormularioRegistroComponent implements OnInit {
         Validators.required
       ]),
       cp: new FormControl('', [
-        Validators.required
+        Validators.required,
+        Validators.pattern(/^(?:0?[1-9]|[1-4]\d|5[0-2])\d{3}$/)
       ]),
       telefono: new FormControl('', [
         Validators.required
       ]),
       fechaNacimiento: new FormControl('', [
-        Validators.required
+        Validators.required,
+        Validators.pattern(/^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})$/),
+
+
       ]),
-      NombreUsuario: new FormControl('', [
-        Validators.required
+      nombreUsuario: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5)
       ]),
-      Email: new FormControl('', [
-        Validators.required
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/)
       ]),
-      Password: new FormControl('', [
-        Validators.required
+      password: new FormControl('', [
+        Validators.required,
+        /* Validators.pattern(/"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"/) //Minimum eight characters, at least one uppercase letter, one lowercase letter and one number: */
       ]),
       repetirPassword: new FormControl('', [
-        Validators.required
+        Validators.required,
+        Validators.pattern(/"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"/)
       ]),
       tatuador: new FormControl('', [
         Validators.required
+      ]),
+      proteccionDatos: new FormControl('', [
+        Validators.required
       ])
-    })
+    }, [this.repeatPasswordValidator]);
 
   }
-
 
   ngOnInit(): void {
   }
 
+
+  /*    adultValidator(form) {
+     const dateOfBirth = parseInt(form.get('fechaNacimiento').value);
+     const actualDate = (new Date());
+     const age = (actualDate - dateOfBirth) / 365 / 24 / 60 / 60 / 1000;
+     if (age < 18) {
+       return false
+     } else null;
+ 
+   } */
+
+
+
+  repeatPasswordValidator(form) {
+    const passwordValue = form.get('password').value;
+    const passwordRepeatValue = form.get('repetirPassword').value;
+    if (passwordValue === passwordRepeatValue) {
+      return null
+    } else {
+      return { repeatPasswordValidator: true }
+    }
+  }
 
 
 
