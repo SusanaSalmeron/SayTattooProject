6ed92157d01ad4cb3bd5b5820fa476c8a073fe22
Sandra <dtena90@gmail.com>
2021-06-17 12:34:01 +0200

    Creado el Componente All_Tatuadores

---
+++ proyectoFinalAngular/src/app/components/all-tatuadores/all-tatuadores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-tatuadores',
+  templateUrl: './all-tatuadores.component.html',
+  styleUrls: ['./all-tatuadores.component.css']
+})
+export class AllTatuadoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
