001ff3e3fb09df06abc541a0e52a55269efdab1c
Sandra <dtena90@gmail.com>
2021-06-17 10:59:57 +0200

    Creación de la página de Simple Card de Tatuadores

--- proyectoFinalAngular/src/app/app-routing.module.ts
+++ proyectoFinalAngular/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormularioRegistroComponent } from './components/formulario-registro/formulario-registro.component';
 
 import { SliderComponent } from './components/slider/slider.component';
+import { CardTatuadoresComponent } from './components/card-tatuadores/card-tatuadores.component'
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: SliderComponent },
-  { path: 'register', component: FormularioRegistroComponent }
+  { path: 'register', component: FormularioRegistroComponent },
+  { path: 'tatuadores/:id', component: CardTatuadoresComponent },
 
 ];
 
--- proyectoFinalAngular/src/app/components/card-tatuadores/card-tatuadores.component.ts
+++ proyectoFinalAngular/src/app/components/card-tatuadores/card-tatuadores.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-card-tatuadores',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardTatuadoresComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('masonry', { static: true }) masonry: ElementRef;
 
-  ngOnInit(): void {
+  constructor(private renderer: Renderer2) { }
+  ngOnInit() { }
+  ngAfterViewInit() {
+    const numCols = 3;
+    const colHeights = Array(numCols).fill(0);
+    Array.from(this.masonry.nativeElement.children).forEach((child: any, i) => {
+      const order = i % numCols;
+      this.renderer.setStyle(child, 'order', order);
+      colHeights[order] += parseFloat(child.clientHeight);
+    })
+    this.renderer.setStyle(this.masonry.nativeElement, 'height', `${Math.max(...colHeights)}px`)
   }
 
 }
+
