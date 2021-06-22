158c322f5d53df574449c96ac8e75e7b210e551e
Sandra <dtena90@gmail.com>
2021-06-22 15:53:01 +0200

    Crear la interface de Tatuador

---
+++ proyectoFinalAngular/src/app/interface.models/tatuador.interface.ts
@@ -0,0 +1,14 @@
+export interface Tatuador {
+    nombre: string;
+    sexo: boolean;
+    direccion: string;
+    cuidad: string;
+    cp: number;
+    telefono: number;
+    fechaNacimiento: string;
+    nombreUsuario: string;
+    email: string;
+    password: string;
+    tatuador: boolean;
+    proteccionDatos: boolean;
+}
